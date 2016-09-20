c417e6dfaa791448e51e6b4f980b1f86a684e513
heemabhatt <heema.bhatt@gmail.com>
2016-09-20 17:06:54 -0400

    Home page with image carousel, contact us form with basic validation and test submit. NOTE: navigation jquery needs repair. TODO: customized validation and database entry in contact us form.

--- TrioWebApp/TrioWebApp/Controllers/TrioHomeController.cs
+++ TrioWebApp/TrioWebApp/Controllers/TrioHomeController.cs
@@ -17,5 +17,14 @@ namespace TrioWebApp.Controllers
         {
             return View();
         }
+
+        public ActionResult TrioContactUs()
+        {
+            return View();
+        }
+        public void submitContactUs(string fullName,string email,string phone, string comment)
+        {
+            Response.Write("Response submitted successfully.<br> NAME: " + fullName + " <br>EMAIL: " +email + " <br>PHONE: " +phone  + " <br>COMMENT: " + comment);
+        }
     }
 }
---
+++ TrioWebApp/TrioWebApp/Models/TrioHomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrioWebApp.Models
+{
+    public class TrioHomeModel
+    {
+        public string fullName {get;set;}
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string comment { get; set; }
+    }
+}
